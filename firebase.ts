36f747257e2e6a059e9b9c65c45cd68c11f73295
Jason Blackburn <jasonblackburn@Jasons-MBP-2.broadband>
2024-09-18 16:22:24 +0100

    project completed

--- firebase.ts
+++ firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQQsCzb7pV4IUJqfJ_TUXrA85Ch5f3ze4",
-  authDomain: "bs-setlist.firebaseapp.com",
-  projectId: "bs-setlist",
-  storageBucket: "bs-setlist.appspot.com",
-  messagingSenderId: "459192821643",
-  appId: "1:459192821643:web:a9ee85b719c050accff18e",
-  measurementId: "G-W1LYH7DC0C",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
